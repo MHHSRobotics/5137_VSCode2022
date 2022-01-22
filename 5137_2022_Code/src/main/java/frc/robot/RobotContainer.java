d16d7219a47c62e4eb3ce2d59a13c1b08188c818
AshB <ashbardh@gmail.com>
2022-01-22 12:19:27 -0800

    Mapped drive base controls. Need to invert specific motors.

--- 5137_2022_Code/src/main/java/frc/robot/RobotContainer.java
+++ 5137_2022_Code/src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveBaseSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -20,10 +18,9 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public static DriveBaseSubsystem driveBase_Subsystem; 
 
-  public final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  public static Command placeHolderCommand;
 
   // Joysticks
   public static Joystick driveController;
@@ -52,6 +49,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return placeHolderCommand;
   }
 }
