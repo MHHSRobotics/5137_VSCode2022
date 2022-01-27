116c9682030509029003468744b6997765f8dfd1
Kianna D <kiannad1000@gmail.com>
2022-01-26 16:01:57 -0800

    added 4 methods to new hang subsystem

---
+++ 5137_2022_Code/src/main/java/frc/robot/subsystems/Hang.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Hang extends SubsystemBase {
+  /** Creates a new Hang. */
+  public Hang() {
+
+    public void hangGoUp(){
+
+    }
+
+    public void hangGoDown(){
+
+    }
+
+    public void hangRotate(){
+
+    }
+
+    public void hangStop(){
+
+    }
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
